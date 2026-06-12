e02b68f32e63502904e3ab216f22224a251f0356
Divanshu Aggarwal <divanshuaggarwal023@gmail.com>
2026-06-12 12:30:12 +0530

    Initial commit: Corrected root directory

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-50">
+      <h1 className="text-4xl font-bold text-blue-600">DevPulse is Live</h1>
+    </div>
+  );
+}
+
+export default App;
